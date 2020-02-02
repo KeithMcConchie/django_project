a87889c30d3e5adc9e1e80bb11a8129835b37866
Keith McConchie <Keith.McConchie@gmail.com>
2020-02-02 12:39:27 -0500

    Part 3 through 18:05.  Added hard-coded dummy post data to views.  Passed to render through context.  Added logic to home template to loop through posts and print them out.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 
+posts = [
+    {'author': 'Keith',
+    'title': 'first post',
+    'content': 'first post content',
+    'date_posted': 'August 27, 2018'
+    },
+    {'author': 'Kevin',
+    'title': 'second post',
+    'content': 'second post content',
+    'date_posted': 'August 28, 2018'
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
