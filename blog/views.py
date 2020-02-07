eac4767d1854c63fb98a675e396fb930f0af86b3
Keith McConchie <Keith.McConchie@gmail.com>
2020-02-07 12:26:15 -0500

    Added template inheritance (part 3 through 26:20)

--- blog/views.py
+++ blog/views.py
@@ -20,4 +20,4 @@ def home(request):
     return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'About'})
+    return render(request, 'blog/about.html', {'title': 'A`bout'})
