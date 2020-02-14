412a70d91a3eb69f18a30c1ee320776844b04c6a
Keith McConchie <Keith.McConchie@gmail.com>
2020-02-14 12:09:33 -0500

    Through end of part 5. Created Post model. Used it to add data to the database.  Changed views to use db. Registered Post model with admin page for edit utility.

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
-
-posts = [
-    {'author': 'Keith',
-    'title': 'first post',
-    'content': 'first post content',
-    'date_posted': 'August 27, 2018'
-    },
-    {'author': 'Kevin',
-    'title': 'second post',
-    'content': 'second post content',
-    'date_posted': 'August 28, 2018'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'A`bout'})
+    return render(request, 'blog/about.html', {'title': 'About'})
